ff79130ffbf0b0a582e328e3d4232a1d80c02969
SeanBitter <paolodig@hotmail.it>
2022-05-24 00:03:22 +0200

    Add the exercise Dadi

---
+++ Dadi/dadi.js
@@ -0,0 +1,36 @@
+/*
+Generare più  numeri random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+C’è forse un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+*/
+
+const dadiComputer = [];
+const dadiPlayer = [];
+let sumComputer = 0;
+let sumPlayer = 0;
+
+for (let i = 1; i < 6; i++) {
+    (dadiComputer[i] = (Math.floor(Math.random() * 6) + 1));
+    dadiComputer.push(dadiComputer[i]);
+    sumComputer += dadiComputer[i];
+    
+    console.log("Il risultato del computer è " + sumComputer);
+}
+
+
+for (let i = 1; i < 6; i++) {
+    (dadiPlayer[i] = (Math.floor(Math.random() * 6) + 1));
+    dadiPlayer.push(dadiPlayer[i]);
+
+    sumPlayer += dadiPlayer[i];
+    
+    console.log("Il tuo risultato è " + sumPlayer);
+}
+
+if (sumPlayer > sumComputer) {
+    console.log("Hai vinto!");
+} else {
+    console.log("Hai perso :(");
+}
